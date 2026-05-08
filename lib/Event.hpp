433461c92d4b129aa6888ae88b7876dc62ef5b64
Daniel Arthur <107006568+MediocreProgrammer89@users.noreply.github.com>
2026-05-08 10:51:36 -0400

    More classes

--- lib/Event.hpp
+++ lib/Event.hpp
@@ -1,1 +1,14 @@
+#ifndef EVENT_HPP
+#define EVENT_HPP
+
+#include "Game.hpp"
+#include "Enemy.hpp"
+
+class Event{
+    public:
+
+    private:
+        
+};
+#endif
 //Dictates the "events" that happen in the game
